6d710cd25685f0c9ffad58725590fd95b051c856
kbrudos <katherine.brudos@hope.edu>
2015-07-16 11:15:59 -0400

    Moving broadcast receiver to it's own class.

---
+++ betaNipperOneAlerter/src/main/java/org/nprlabs/nipperone/main/MyReceiver.java
@@ -0,0 +1,16 @@
+package org.nprlabs.nipperone.main;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by kbrudos on 7/15/2015.
+ */
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+    }
+}
